5b1d74c6e97dc77407201cae8a5d3ff461e4ca7a
Meruzhan Gasparyan <meruzhan.gasparyan@arm.synisys.com>
2017-05-18 18:47:19 +0400

    adding implimentation  bus number rooms

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -10,19 +9,41 @@ app.get('/', function (req, res) {
 });
 
 
+var buses = [{busNumber: "11", cor: {x: 10, y: 10}}, {busNumber: "12", cor: {x: 20, y: 20}}, {
+    busNumber: "13",
+    cor: {x: 30, y: 30}
+},
+    {busNumber: "14", cor: {x: 40, y: 40}}, {busNumber: "15", cor: {x: 50, y: 50}}, {
+        busNumber: "16",
+        cor: {x: 60, y: 60}
+    }];
+
 /**
  * connect confirm withe users
  */
 io.on('connection', function (socket) {
 
+    socket.on("joinRoom", function (data, cb) {
+        console.log(data);
+        socket.join(data.busNumber);
+        cb && cb("join number = " + data.busNumber);
+    });
+
+
+    socket.on("coreBus", function (data) {
+        console.log(data);
+        io.sockets.in(data.busNumber).emit("coreBusNumber", data);
+    });
+
     /**
-     * 
+     *
      */
     socket.on('news', function (data, cb) {
+        console.log(io.sockets);
         socket.broadcast.send('news', data);
         cb("ok");
         console.log(data);
-
+        console.log(socket.handshake);
     });
 });
 
